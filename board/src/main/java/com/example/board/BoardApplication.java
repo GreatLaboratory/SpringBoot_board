a17170cd466288a3152dc14adce647e888e7193e
Myung-gwan Kim <cartopia95@naver.com>
2019-07-14 20:40:49 +0900

    controller config

--- board/src/main/java/com/example/board/BoardApplication.java
+++ board/src/main/java/com/example/board/BoardApplication.java
@@ -3,8 +3,6 @@ package com.example.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude= {MultipartAutoConfiguration.class})
 public class BoardApplication {
